b46211d71cfa9e190bf3ad1d5f498e4980488d77
Neo <Neo-7@users.noreply.github.com>
2018-11-03 11:10:17 +0530

    Update Assets/scripts/script.js

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,56 +1,5 @@
 
 // ripple
-  /*
-    (function() { var cleanUp, debounce, i, len, ripple, rippleContainer, ripples, showRipple;
-
-      debounce = function(func, delay) {
-          var inDebounce;
-          inDebounce = undefined;
-          return function() {
-              var args, context;
-              context = this;
-              args = arguments;
-              clearTimeout(inDebounce);
-              return inDebounce = setTimeout(function() {
-                  return func.apply(context, args);
-              }, delay);
-          };
-      };
-      
-      showRipple = function(e) {
-          var pos, ripple, rippler, size, style, x, y;
-          ripple = this;
-          rippler = document.createElement('span');
-          size = ripple.offsetWidth;
-          pos = ripple.getBoundingClientRect();
-          x = e.pageX - pos.left - (size / 2);
-          y = e.pageY - pos.top - (size / 2);
-          style = 'top:' + y + 'px; left: ' + x + 'px; height: ' + size + 'px; width: ' + size + 'px;';
-          ripple.rippleContainer.appendChild(rippler);
-          return rippler.setAttribute('style', style);
-      };
-      
-      cleanUp = function() {
-          while (this.rippleContainer.firstChild) {
-            this.rippleContainer.removeChild(this.rippleContainer.firstChild);
-          }
-      };
-      
-      ripples = document.querySelectorAll('[data-ripple="true"]');
-      
-      for (i = 0, len = ripples.length; i < len; i++) {
-          ripple = ripples[i];
-          rippleContainer = document.createElement('div');
-          rippleContainer.className = 'ripple';
-          ripple.addEventListener('mousedown', showRipple);
-          ripple.addEventListener('mouseup', debounce(cleanUp, 2000));
-          ripple.rippleContainer = rippleContainer;
-          ripple.appendChild(rippleContainer);
-      }
-    }());
-    */
-
-// ripple
     var rippleSection = document.querySelectorAll('[data-ripple="true"]');
 
     for (var i = 0; i < rippleSection.length; i++) {
@@ -84,39 +33,4 @@
                 rippleDiv.parentElement.removeChild(rippleDiv);
             }, 1500);
         }
-
     }
-
-    
-    
-    /*
-    $('body').on('click', '[data-ripple="true"]', function (e) {
-        var eleRippleBG = $(this).css('background-color');
-        var eleRippleColor = $(this).css('color');
-        var rippleColor;
-
-        //console.log(eleRipple);
-        if(eleRippleBG == 'rgba(0, 0, 0, 0)' || eleRippleBG == 'rgb(255, 255, 255)'){
-            rippleColor = eleRippleColor
-        }
-        else{
-            rippleColor = 'rgb(255,255,255)'
-        }
-
-        var rippleDiv = $('<div class="ripple" />'),
-            rippleOffset = $(this).offset(),
-            rippleY = e.pageY - rippleOffset.top,
-            rippleX = e.pageX - rippleOffset.left,
-            ripple = $('.ripple');
-
-        rippleDiv.css({
-            top: rippleY - (ripple.height() / 2),
-            left: rippleX - (ripple.width() / 2),
-            background: rippleColor
-        }).appendTo($(this));
-
-        window.setTimeout(function () {
-            rippleDiv.remove();
-        }, 1500);
-    });
-    */
